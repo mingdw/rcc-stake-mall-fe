c93b07812e9c822bdfe12158c505dae01f45d4ec
mingdw <466830255@qq.com>
2025-01-19 22:29:57 +0800

    aaa

--- src/view/suply/SuplyIndex.tsx
+++ src/view/suply/SuplyIndex.tsx
@@ -1,10 +1,15 @@
 import React,{FC} from "react";
-
+import styles from './Suply.module.scss';
 
 const Suply: FC = () => {
     return <>
-        <div>
-            质押服务首页
+        <div className={styles.containerTop}>
+           <div className={styles.containerTopLeft}>
+                <h2>以太坊主网</h2>
+           </div>
+           <div className={styles.containerTopRight}>
+            
+            </div>
         </div>
     </>
 }
